38aa0d67d254252c76145ca905b553478f8aa666
ChrisLee <liyunfeng1989@126.com>
2017-09-15 13:21:08 +0800

    Mocker Project

---
+++ src/renderer/App.vue
@@ -0,0 +1,127 @@
+<template>
+  <v-app
+          dark
+          id="app"
+          toolbar
+  >
+    <v-navigation-drawer
+            class="pb-0"
+            persistent
+            height="100%"
+            clipped
+            absolute
+            enable-resize-watcher
+            permanent
+            v-model="drawer"
+    >
+      <v-list dense>
+        <v-list-tile v-for="item in items" :key="item.text" @click="">
+          <v-btn block primary>新建项目</v-btn>
+        </v-list-tile>
+        <v-subheader class="mt-3 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+        <v-list>
+          <v-list-tile v-for="item in items2" :key="item.text" avatar @click="">
+            <v-list-tile-avatar>
+              <img :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`" alt="">
+            </v-list-tile-avatar>
+            <v-list-tile-title v-text="item.text"></v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+        <v-list-tile class="mt-3" @click="">
+          <v-list-tile-action>
+            <v-icon class="grey--text text--darken-1">add_circle_outline</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Browse Channels</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon class="grey--text text--darken-1">settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Manage Subscriptions</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar class="deep-purple accent-4" fixed>
+      <v-toolbar-title>
+        <v-btn icon large>
+          <v-icon large>play_arrow</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>favorite</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <main>
+      <v-container fill-height>
+        <v-layout>
+          <v-flex xs12>
+            <v-card height="300px"></v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'mocker',
+    data(){
+      return {
+        drawer: true,
+        items: [
+          { icon: 'trending_up', text: 'Most Popular' },
+          { icon: 'subscriptions', text: 'Subscriptions' },
+          { icon: 'history', text: 'History' },
+          { icon: 'featured_play_list', text: 'Playlists' },
+          { icon: 'watch_later', text: 'Watch Later' }
+        ],
+        items2: [
+          { picture: 28, text: 'Joseph' },
+          { picture: 38, text: 'Apple' },
+          { picture: 48, text: 'Xbox Ahoy' },
+          { picture: 58, text: 'Nokia' },
+          { picture: 78, text: 'MKBHD' },
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  html{
+    overflow:hidden !important;
+  }
+
+  #app{
+    position:relative;
+  }
+
+  @import '../../node_modules/material-design-icons/iconfont/material-icons.css'
+  @import '../../node_modules/vuetify/src/stylus/settings/_colors'
+
+  $theme := {
+    primary: $red.darken-2
+    accent: $red.accent-2
+    secondary: $grey.lighten-1
+    info: $blue.lighten-1
+    warning: $amber.darken-2
+    error: $red.accent-4
+    success: $green.lighten-2
+  }
+
+  @import '../../node_modules/vuetify/src/stylus/main'
+</style>
